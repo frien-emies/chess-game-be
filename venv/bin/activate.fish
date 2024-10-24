979b996d886e7e314b2f645f97836263aadc936d
Jan Lehigh <janlehigh461437@gmail.com>
2024-10-24 14:52:01 -0600

    Add function for the new_game endpoint

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/seth/turing_work/4mod/chess-game-be/venv"
+set -gx VIRTUAL_ENV "/Users/janlehigh/turing/4mod/fren-emies/chess-game-be/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
